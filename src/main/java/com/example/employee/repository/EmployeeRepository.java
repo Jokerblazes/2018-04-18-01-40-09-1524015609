a4c99711927e550101dfd9590062f9a2144590c9
Jokerblazes <zcd123@qq.com>
2018-05-05 14:54:25 +0800

    修改第一次实现

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -13,13 +13,11 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
     //1.查询名字是*的第一个employee
-    @Query(value = "select e from Employee e where e.name=:name")
     public Page<Employee> findEmployeeByName(@Param("name") String name, Pageable pageable);
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
     @Query(value = "select e from Employee e where e.name like CONCAT('%',:name,'%') and e.salary > :salary")
     public Page<Employee> findEmployeeByCharacterAndSalary(@Param("name") String name,@Param("salary") int salary,Pageable pageable);
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
-
     //4.实现对Employee的分页查询，每页两个数据
 
     //5.查找**的所在的公司的公司名称
