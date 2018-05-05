4d0c6be249ce87eb5a6d209a4577d2955a58b720
Jokerblazes <zcd123@qq.com>
2018-05-05 15:43:06 +0800

    4.实现对Employee的分页查询，每页两个数据

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -20,6 +20,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
     public Employee findDistinctFirstByCompanyIdOrderBySalaryDesc(@Param("companyId") long companyId);
+
     //4.实现对Employee的分页查询，每页两个数据
     //5.查找**的所在的公司的公司名称
 
